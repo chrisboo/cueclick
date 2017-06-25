6265a1bc2621b72639738c82d479cc833af733fe
Joanne <joanneong225@gmail.com>
2017-06-25 14:01:47 +0800

    Add style for new presentation button

--- public/assets/js/homePage.js
+++ public/assets/js/homePage.js
@@ -1,142 +1,134 @@
-// Enter an API key from the Google API Console:
-//   https://console.developers.google.com/apis/credentials
-var apiKey = 'AIzaSyCSjg3rrx6Obl4ngZsDlFlV4degUJSMvbw';
-// Enter the API Discovery Docs that describes the APIs you want to access
-var discoveryDocs = ["https://slides.googleapis.com/$discovery/rest?version=v1"];
-// Enter a client ID for a web application from the Google API Console:
-//   https://console.developers.google.com/apis/credentials?project=_
-// In your API Console project, add a JavaScript origin that corresponds
-//   to the domain where you will be running the script
-var clientId = '408869653199-ruoft30vmoqrgpku3us3qd2leb3k6tp1.apps.googleusercontent.com';
-// Enter one or more authorization scopes. Refer to the documentation for
-// the API or https://developers.google.com/people/v1/how-tos/authorizing
-// for details
-var scopes = 'https://www.googleapis.com/auth/presentations.readonly https://www.googleapis.com/auth/drive';
-
-var authorizeButton = document.getElementById('authorize-button');
-var signoutButton = document.getElementById('signout-button');
-var center = document.getElementById('center');
-var user;
-var authResponse;
-var oauthToken;
-var pickerApiLoaded = false;
-var presentationId;
-var presentation;
-
-function handleClientLoad() {
-    // Load the API client and auth2 library
-    gapi.load('client:auth2', initClient);
-    //Load the Picker API
-    gapi.load('picker', onPickerApiLoad);
+/* -- Background image -- */
+body {
+    background-image: url(../img/wood.png);
+    background-repeat: no-repeat;
+    background-size: cover;
+    background-attachment: fixed;
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+    height: 100vh;
 }
 
-function initClient() {
-    gapi.auth2.init({
-        apiKey: apiKey,
-        discoveryDocs: discoveryDocs,
-        clientId: clientId,
-        scope: scopes
-    }).then(function () {
-      // Listen for sign-in state changes
-      gapi.auth2.getAuthInstance().isSignedIn.listen(updateSigninStatus);
-      // Handle the initial sign-in state
-      updateSigninStatus(gapi.auth2.getAuthInstance().isSignedIn.get());
-      // Set the current Google User
-      gapi.auth2.getAuthInstance().currentUser.listen(updateUser);
-      authorizeButton.onclick = handleAuthClick;
-      signoutButton.onclick = handleSignoutClick;
-  });
+/* -- Login element -- */
+#center {
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+    background-color: rgba(255, 255, 255, 0.7);
+    border-radius: 15%;
+    box-shadow: 2px 2px 4px 3px #505050;
+    width: 30%;
+    height: auto;
+    position: absolute;
 }
 
-// Callback to make sure that the Picker API has loaded
-function onPickerApiLoad() {
-  pickerApiLoaded = true;
-  createPicker();
+/* -- CueClick Logo -- */
+#logo {
+    max-width: 80%;
+    width: 80%;
+    height: auto;
+    position: relative;
 }
 
-function updateSigninStatus(isSignedIn) {
-    if (isSignedIn) {
-      center.style.display = 'none';
-      signoutButton.style.display = 'block';
-      createPicker();
-    } else {
-      signoutButton.style.display = 'none';
-    }
+#para {
+    position: relative;
+    font-family: Palatino, "Palatino Linotype", "Palatino LT STD", "Book Antiqua", Georgia, serif;
+    font-size: 2vw;
+    max-width: 100%;
+    height: auto;
+    line-height: 2%;
 }
 
-// Store the current Google user
-function updateUser(gUser) {
-    user = gUser;
-    // If the user is a Google User, update the user token
-    if (user.Zi != null) {
-        updateToken();
-    }
+/* -- Sign in button -- */
+#authorize-button {
+    position: relative;
+    font-family: Palatino, "Palatino Linotype", "Palatino LT STD", "Book Antiqua", Georgia, serif;
+    font-size: 1.5vw;
+    margin-bottom: 2%;
 }
 
-// Store the access token
-function updateToken() {
-  authResponse = user.getAuthResponse(true);
-  oauthToken = authResponse.access_token;
+#authorize-button:hover {
+    cursor: pointer;
 }
 
-function handleAuthClick(event) {
-    gapi.auth2.getAuthInstance().signIn();
+/* -- Style for button overlays -- */
+.overlay {
+    right: 1.5%;
+    width: 4.5vw;
+    height: 4.5vw;
+    border: 0;
+    border-radius: 50%;
+    z-index: 2;
+    color: #5b7193;
+    opacity: 0;
 }
 
-function handleSignoutClick(event) {
-    gapi.auth2.getAuthInstance().signOut();
-    // Reload the page on signout
-    window.location.reload();
+.overlay:hover {
+    opacity: 0.4;
+    transition: 200ms ease-out;
 }
 
-// Create and render a Picker object for picking user slides
-function createPicker() {
-    if (pickerApiLoaded && oauthToken) {
-      var picker = new google.picker.PickerBuilder().
-        addView(google.picker.ViewId.PRESENTATIONS).
-        setOAuthToken(oauthToken).
-        setDeveloperKey(apiKey).
-        setCallback(pickerCallback).
-        build();
-      picker.setVisible(true);
-    }
+/* -- Style for side buttons -- */
+#side-buttons {
+    position: fixed;
+    bottom: 1.5%;
+    right: 1.5%;
+    width: 4.5vw;
+    height: 11vw;
 }
 
-// Callback implementation - get the Presentation object using its ID
-function pickerCallback(data) {
-      if(data[google.picker.Response.ACTION] == google.picker.Action.PICKED) {
-        var doc = data[google.picker.Response.DOCUMENTS][0];
-        presentationId = doc[google.picker.Document.ID];
-        getPresentation();
-    }
+/* -- New presentation button -- */
+#new-presentation {
+    position: absolute;
+    display: none;
+    top: 0;
+    width: 4.5vw;
+    height: 4.5vw;
 }
 
-// Get the presentation by making a GET request to the Google Slides API
-// and store the Presentation object in a variable
-function getPresentation() {
-    // Create a new XMLHttpRequest to get the corresponding slides object
-    var xhr = new XMLHttpRequest(),
-        method = 'GET',
-        url = "https://slides.googleapis.com/v1/presentations/" + presentationId;
-    xhr.open(method, url, true);
-    xhr.setRequestHeader('Authorization', 'Bearer ' + oauthToken);
-    xhr.onreadystatechange = function() {
-        if (xhr.readyState === XMLHttpRequest.DONE && xhr.status === 200) {
-            // Get the JSON object (the Presentation) from the returned string
-            presentation = JSON.parse(xhr.responseText);
-            displayPresentation();
-        }
-    }
-    xhr.send();
+#bottom-presentation {
+    position: absolute;
+    top: 0;
+    width: 4.5vw;
+    height: 4.5vw;
+    background-color: rgba(255, 255, 255, 0.7);
+    border-radius: 50%;
+    border: 0;
+    font-size: 4vw;
+    font-family: "Arial Rounded MT Bold", "Helvetica Rounded", Arial, sans-serif;
 }
 
-// Display the chosen presentation in an iframe
-function displayPresentation() {
-    // Display the presentation in an iframe
-    var iframe = document.getElementById('slides');
-    iframe.style.display = "block";
-    iframe.src = "https://docs.google.com/presentation/d/" + presentation.presentationId + "/embed?start=false&loop=false&delayms=3000";
-    iframe.onload = function() {
-        initWebControl();
-    }
+#top-presentation {
+    position: absolute;
+    top: 0;
+}
+
+/* -- Sign out button -- */
+#signout-button {
+    position: absolute;
+    display: none;
+    bottom: 0;
+    width: 4.5vw;
+    height: 4.5vw;
+}
+
+#bottom-button {
+    position: absolute;
+    bottom: 0;
+    width: 4.5vw;
+    height: 4.5vw;
+    z-index: 1;
+}
+
+#top-button {
+    position: absolute;
+    bottom: 0;
+}
+
+/* -- iframe -- */
+#slides {
+    display: none;
 }
